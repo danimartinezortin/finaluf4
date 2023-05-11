ae9a506a3ff4cc01fabae285c5a53d9e0f09965e
Daniel Martinez <dmartinez@institutmvm.cat>
2023-05-11 18:26:36 +0200

    added fernando alonso el mas veloz

--- index.php
+++ index.php
@@ -3,8 +3,7 @@ $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 include "hola.php";
 echo “Listado de personas:”
 include "persona.php";
-
 include "persona2.php";
 include "persona3.php";
+include “persona4.php”;
 ?>
-
